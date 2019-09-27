a7a81581852eed38682c0105eebebcba88204bb8
Ubuntu <ubuntu@ip-172-31-27-44.ec2.internal>
2019-09-27 11:30:58 +0000

    Created index view and base.html

--- template_store/views.py
+++ template_store/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Template
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    
+    queryset =  Template.objects.all()
+    context = { 'object_list' : queryset }
+    
+    return render(request, 'base.html', context)
