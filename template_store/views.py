210cc23636505acc7db10cee61afef18f1281930
Ubuntu <ubuntu@ip-172-31-27-44.ec2.internal>
2019-09-30 11:48:49 +0000

    Added styling for landing page

--- template_store/views.py
+++ template_store/views.py
@@ -5,8 +5,9 @@ from .models import Template
 
 
 def index(request):
-    
     queryset =  Template.objects.all()
-    context = { 'object_list' : queryset }
+    context = { 'object_list' : queryset,
+                'title': 'List'
+    }
     
     return render(request, 'base.html', context)
